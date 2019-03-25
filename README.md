8ad7891fe53d869ab09d2010ac6e37c5b2f8e83f
Bjørn Tore Svinningen <postmannen@gmail.com>
2019-03-25 18:21:03 +0100

    document, and removed not needed code.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# lexmlparser
+# lexmlparser, *UNDER CONSTRUCTION*
+
 Parser for the tokens from the lexml package. Under construction.
+
+## What it does
+
+This package takes the tokens produced by the lexml package and creates a Go struct of the parsed values
--- parser.go
+++ parser.go
@@ -1,4 +1,6 @@
 /*
+	This package takes the tokens produced by the lexml package and creates a Go struct of the parsed values
+
 	tokenStartTag      TokenType = "tokenStartTag"      // <tag> || <
 	tokenEndTag        TokenType = "tokenEndTag"        // </tag> || />
 	tokenArgumentFound TokenType = "tokenArgumentFound" // =
@@ -30,51 +32,42 @@ var depthMap map[int]string = map[int]string{
 	9: "									",
 }
 
-//Start will start the lexml parser
-
+//Start will start the lexml parser. Takes a channel of tokens as it's input.
 func Start(tCh chan lexml.Token) {
 	depth := 0             //used to indicate what level or sub level we are in the struct/tag
 	literal := []string{}  //used for a single literal
 	literals := []string{} //used for storing all the literals
 
 	for v := range tCh {
-		//fmt.Println("* depth = ", depth)
 		switch v.TokenType {
 		case "tokenStartTag":
 			if depth == 0 {
-				//fmt.Printf("*DEPTH=%v* type %v struct {\n", depth, v.TokenText)
 				fmt.Printf("type %v struct {\n", v.TokenText)
 				literals = append(literals, fmt.Sprintf("var %v %v", v.TokenText, v.TokenText))
 				literal = append(literal, fmt.Sprintf("%v", v.TokenText))
 			}
 			if depth > 0 {
-				//fmt.Printf("*DEPTH=%v* %v %v struct {\n", depth, depthMap[depth], v.TokenText)
 				fmt.Printf("%v %v struct {\n", depthMap[depth], v.TokenText)
 				literal = append(literal, v.TokenText)
 			}
 			depth++
 		case "tokenEndTag":
 			depth--
-			//fmt.Printf("*DEPTH=%v* %v }\n", depth, depthMap[depth])
 			fmt.Printf("%v }\n", depthMap[depth])
 			literal = literal[:depth]
 		case "tokenArgumentFound":
 		case "tokenArgumentName":
 			depth++
-			//fmt.Printf("*DEPTH=%v* %v %v", depth, depthMap[depth], v.TokenText)
 			fmt.Printf("%v %v", depthMap[depth], v.TokenText)
 			literal = append(literal, v.TokenText)
 			depth--
 		case "tokenArgumentValue":
 			fmt.Printf(" %v\n", "string")
 			literal = append(literal, v.TokenText)
-			//fmt.Println(literal)
-			//fmt.Println("literal = ", literal)
 			literals = append(literals, createLiteral(literal))
 			literal = literal[:depth]
 
 		case "tokenDescription":
-			//fmt.Println(v.TokenText)
 		case "tokenEOF":
 		case "tokenJustText":
 		}
@@ -84,6 +77,7 @@ func Start(tCh chan lexml.Token) {
 	}
 }
 
+//createLiterals will create a literal string. Takes []string and return a string to the caller.
 func createLiteral(s []string) string {
 	var tmpString string
 
